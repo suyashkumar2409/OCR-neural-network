b8c936381ad170d9eb4e844d5b99a155121329a8
SuyashKumar <f2014053@pilani.bits-pilani.ac.in>
2016-05-24 12:21:39 +0530

    Completed backpropagation algorithm wrt quadratic cost

--- quadcost.py
+++ quadcost.py
@@ -1,5 +1,15 @@
 import numpy as np
 import random
+
+def sigmoid(x):
+	return 1.0/(1.0+np.exp(-x))
+
+def sigmoidderiviative(x):
+	temp = sigmoid(x)
+
+	return (1.0-temp)*temp
+
+
 class Network(object):
 	def __init__(self,sizes):
 	
@@ -56,4 +66,42 @@ class Network(object):
 		self.biases = [b - (eta/len(mini)*db for b,db in zip(self.biases,biastemp)]
 		self.weights = [w - (eta/len(mini)*dw for w,dw in zip(self.weights,weighttemp)]
 
+	def backprop(self,x,y):
+
+		bret = [np.zeros(b.shape) for b in self.biases]
+		wret = [np.zeros(w.shape) for w in self.weights]
+
+		activations = [x]
+		zs = []
+
+		lastactivation = x
+		for b,w in zip(self.biases,self.weights):
+			z = np.dot(w,lastactivation) + b
+			a = sigmoid(z)
+
+			activations.append(a)
+			zs.append(z)
+
+			lastactivation = a
+
+		deltas = [np.zeros(a.shape) for a in activations]
+
+		deltal = (activations[-1]-y)*sigmoidprime(zs[-1])
+		
+		bret[-1] = deltal
+
+		wret[-1] = np.dot(deltal,activations[-2].transpose())
+
+		deltas[-1] = deltal
+
+		for i = xrange(2,len(self.weights)):
+			deltacurr = np.dot(self.weights[-l+1].transpose,deltas[-l+1])*sigmoidprime(zs[-l])
+			deltas[-l] = deltacurr
+
+			bret[-l] = deltacurr
+
+			wret[-l] = np.dot(deltacurr,activations[-l-1].transpose())
+
+		return bret,wret
+				
 
