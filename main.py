42b3f4ddffbf3948330471e455e38f30a51c12ac
SuyashKumar <f2014053@pilani.bits-pilani.ac.in>
2016-05-25 11:49:11 +0530

    added regularization but accuracy is not good

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import quadcost
 import crosscost
 import mnistloader
+import crossregularization
 
 trainingdata, validationdata, testingdata = mnistloader.load_data_wrapper()
 
-net = crosscost.Network([784,30,10])
+net = crossregularization.Network([784,30,10])
 
-net.graddesc(trainingdata,30,10,0.02,testingdata)
+net.graddesc(trainingdata,30,10,2.0,testingdata,lamda=1.0)
 
 
 #####Use learning rate 3 when using quadcost, and 0.005 while using crosscost for best results and avoiding under/overfitting
